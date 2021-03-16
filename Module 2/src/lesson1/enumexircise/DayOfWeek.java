b55388e6fd5e81c0f1b53651088b302839627974
Renaon <egorbaturin2013@yandex.ru>
2021-03-16 15:59:46 +0300

    First homework

--- Module 2/src/lesson1/enumexircise/DayOfWeek.java
+++ Module 2/src/lesson1/enumexircise/DayOfWeek.java
@@ -1,4 +1,20 @@
 package lesson1.enumexircise;
 
 public enum DayOfWeek {
+    SUNDAY("Воскресенье", 0), SATURDAY("Суббота", 0),
+     FRIDAY("Пятница", 0), THURSDAY("Четверг", 8),
+    WEDNSDAY("Среда", 8), TUESDAY("Вторник", 8),
+    MONDAY("Понедельник", 8);
+    
+    private String russianTitle;
+    private int hours;
+
+    DayOfWeek(String russianTitle, int hours) {
+        this.russianTitle = russianTitle;
+        this.hours = hours;
+    }
+
+    public int getHours(){
+        return this.hours;
+    }
 }
