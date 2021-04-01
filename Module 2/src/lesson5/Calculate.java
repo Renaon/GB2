87e0416ed95b3bdd275db1b57a298c9fea97fbc9
Renaon <egorbaturin2013@yandex.ru>
2021-04-01 16:05:19 +0300

    I have done!

--- Module 2/src/lesson5/Calculate.java
+++ Module 2/src/lesson5/Calculate.java
@@ -1,20 +1,42 @@
 package lesson5;
 
-public class Calculate {
+public class Calculate{
     final int h;
-    private float a1[];
-    private float a2[];
+    private final float a1[];
+    private final float a2[];
+    private int size;
+    private final float[] arr;
 
-    public Calculate(int size){
+
+
+    public Calculate(int size,float[] arr ){
+        this.size = size;
         this.h = size/2;
         this.a1 = new float[h];
         this.a2 = new float[h];
-
+        this.arr = arr;
     }
 
-    public void delTwo(float[] arr){
-        System.arraycopy(arr, 0, a1, 0, h);
-        System.arraycopy(arr, h, a2, 0, h);
+    public synchronized void calA1(){
 
+        System.arraycopy(arr, h, a2, 0, h);
+        for(int i = 0; i<h; i++){
+            a2[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+
+    public synchronized void calA2(){
+        System.arraycopy(arr, 0, a1, 0, h);
+        for(int i = 0; i<h; i++){
+            a1[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+
+    public float[] getA1() {
+        return a1;
+    }
+
+    public float[] getA2() {
+        return a2;
     }
 }
--- Module 2/src/lesson5/Main.java
+++ Module 2/src/lesson5/Main.java
@@ -2,11 +2,14 @@ package lesson5;
 
 public class Main extends Thread{
     static final int size = 10000000;
+    static int h = size/2;
 
 
 
     public static void main(String[] args){
-
+        numberOne();
+        System.out.println();
+        numberTwo();
     }
 
     private static void numberOne(){
@@ -22,15 +25,27 @@ public class Main extends Thread{
         System.out.println(System.currentTimeMillis() - a);
     }
 
-    @Override
-    public void run(){
-
-    }
 
     private static void numberTwo(){
+
         float[] arr = new float[size];
+        for(int i = 0; i<size; i++){
+            arr[i] = 1;
+        }
+        Calculate a1 = new Calculate(size,arr);
+        Calculate a2 = new Calculate(size,arr);
+        long a = System.currentTimeMillis();
 
+        new Thread(() -> a1.calA1()).start();
+        new Thread(() -> a2.calA2()).start();
 
+        float[] arr1 = a1.getA1();
+        float[] arr2 = a2.getA2();
+        System.arraycopy(arr1, 0, arr, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+        System.out.println(System.currentTimeMillis() - a);
 
     }
+
+
 }
